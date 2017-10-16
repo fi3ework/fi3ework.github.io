9047995820c6ea60cc150b7006542560f9666167
Wee <fi3ework@gmail.com>
2017-10-16 16:52:58 +0800

    Site updated: 2017-10-16 16:52:57

--- scripts/init.js
+++ scripts/init.js
@@ -7,9 +7,15 @@ var init = function init() {
     var $introImg = $('.site-intro-img:first'),
         introPlaceholder = $('.site-intro-placeholder:first'),
         bgCSS = $introImg.css('background-image'),
-        bgURL = bgCSS.match(/url\("(.*)"\)/)[1];
+        bgRegResult = bgCSS.match(/url\("*([^"]*)"*\)/);
 
-    var img = new Image();
+    if (bgRegResult.length < 2) {
+        console.log(bgRegResult);
+        return;
+    }
+
+    var bgURL = bgRegResult[1],
+        img = new Image();
     img.onload = function () {
         introPlaceholder.remove();
         console.info('PLACEHOLDER REMOVED');
@@ -20,7 +26,9 @@ var init = function init() {
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOMContentLoaded');
     document.getElementsByClassName('container')[0].classList.remove('container-unloaded');
+    document.getElementsByClassName('container')[0].classList.add('container-footer-fade-in');
     document.getElementsByClassName('footer')[0].classList.remove('footer-unloaded');
+    document.getElementsByClassName('footer')[0].classList.add('container-footer-fade-in');
     document.getElementsByClassName('loading')[0].style.display = 'none';
 }, false);
 
